2f369f89937047e383be2805eb4dcafc7eebbfac
rauffaizov <rauf.faizov.200271@gmail.com>
2024-11-18 09:02:13 +0100

    Add input buttons to more user friendly api

--- app/main.py
+++ app/main.py
@@ -7,10 +7,11 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app.bookings.router import router as booking_router
 from app.users.router import router as users_router
+from app.pages.router import router as pages_router
 
 app = FastAPI()
 app.include_router(users_router)
 app.include_router(booking_router)
-
+app.include_router(pages_router)
 
 
--- app/users/router.py
+++ app/users/router.py
@@ -12,14 +12,15 @@ router = APIRouter(
 )
 #Registering of Users and checking if this user exist or not in DB
 @router.post("/register")
-async def register_user(user_data:SUserRegister):
+async def register_user(user_email, password)->SUserRegister:
     #This sentence checking if the user exists
-    existing_user = await BaseDAO.find_one_or_none(Users,email = user_data.email)
+    existing_user = await BaseDAO.find_one_or_none(Users,email = user_email)
     if existing_user:
         raise UserAlreadyExistException
     #if not we will add him in DB
-    hashed_password = get_password_hash(user_data.password)
-    await BaseDAO.add(Users,email = user_data.email, hashed_password = hashed_password)
+    hashed_password = get_password_hash(password)
+    await BaseDAO.add(Users,email = user_email, hashed_password = hashed_password)
+    return {"email":user_email, "password":hashed_password}
 
 @router.post("/login")
 async def login_user(response: Response, user_data:SUserAuth):
